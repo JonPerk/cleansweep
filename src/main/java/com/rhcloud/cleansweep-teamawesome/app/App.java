539960bfd5010f0391c5e4e1c455412916d80acb
Jon Perkowski <perkowski.jon@gmail.com>
2015-10-03 16:03:39 -0500

    intial project setup

---
+++ src/main/java/com/rhcloud/cleansweep-teamawesome/app/App.java
@@ -0,0 +1,13 @@
+package com.rhcloud.cleansweep-teamawesome.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
